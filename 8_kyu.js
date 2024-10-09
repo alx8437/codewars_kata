f291e455303b921a112e1219d5c1be2748e17af1
Aleks Zelenskiy <dezion777@gmail.com>
2024-10-09 11:34:27 +0300

    task:  Removing Elements

--- 8_kyu.js
+++ 8_kyu.js
@@ -47,3 +47,14 @@ function countSheep(sheep) {
     return result;
 }
 
+// Removing Elements
+function removeEveryOther(arr){
+    const result = [];
+
+    for (let i = 0; i < arr.length; i += 2) {
+        result.push(arr[i]);
+    }
+
+    return result
+}
+
