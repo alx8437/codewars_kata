5f8fc12fa6b54fc5a06a5154aad5d42872c1e38b
Aleks Zelenskiy <dezion777@gmail.com>
2025-09-30 14:23:05 +0300

    7_kata, Find Your Villain Name

--- 7_kyu.js
+++ 7_kyu.js
@@ -31,6 +31,28 @@ function getVillainName(birthday){
     return `The ${m[month]} ${d[date]}`
 }
 
+// Sum of two lowest positive integers
+function sumTwoSmallestNumbers(numbers) {
+    const sorted = numbers.sort((a, b) => a - b);
+
+    return sorted[0] + sorted[1];
+}
+
+function factorial(n) {
+    if (n < 0 || n >= 12) {
+        throw new RangeError('Input must be between 0 and 12')
+    }
+
+    if (n === 0) return 1
+
+    let result = 1;
+
+    for (let i = 1; i <= n; i++) {
+        result *= i
+    }
+
+    return result
+}
 
 
 
